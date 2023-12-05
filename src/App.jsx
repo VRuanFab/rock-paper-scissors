360f74f6f065911dfa4b607c05dc4eb547a976d2
Victor Ruan Fabricio Cavalcante <victor.ruanfab@gmail.com>
2023-12-05 13:04:59 -0300

    atualizando joguinho

--- src/App.jsx
+++ src/App.jsx
@@ -7,31 +7,73 @@ import scissors from './images/scissors.png'
 
 function App() {
 
-  const [click, setClick] = useState(defaultImage)
+  const [oponent, setOponent] = useState(defaultImage)
+  const [escolha, setEscolha] = useState([])
 
-  function clickou(){
+  function click(){
     const choice = Math.floor(Math.random() * 3 + 1)
     switch (choice){
       case 1:
-          setClick(paper)
+          setOponent(paper)
         break;
       
       case 2:
-          setClick(rock)
+          setOponent(rock)
         break;
 
       case 3:
-          setClick(scissors)
+          setOponent(scissors)
         break;
     }
-    console.log(choice)
+
+      const pedra = document.getElementById('Pedra')
+      const papel = document.getElementById('Papel')
+      const tesoura = document.getElementById('Tesoura')
+  
+      switch (pedra){
+        case choice === 1:
+          console.log('you win')
+          break;
+      }
   }
+
+  const style = ['w-[10rem] h-[9rem]']
 
   return (
     <div className=''>
-      Eae
-      <img src={click} alt='papel' className='w-[7rem] h-[6rem]'/>
-      <input type='button' value='Aperte' onClick={clickou} className='border-2 p-1 bg-[#f0f0f0]'/>
+      <h1 className='flex justify-center mt-3'>Pedra, papel, tesoura</h1>
+
+      <div className='flex justify-center mt-10'>
+        <img src={oponent} alt='papel' className={style[0]}/>
+        {escolha === null? (<></>):(
+          <img src={escolha} alt='escolha'/>
+        )}
+      </div>
+
+      <div>
+        Escolha:
+        <div className='flex space-x-10'>
+
+          <label for='Pedra'>
+            <img src={rock} alt='Pedra' className={style[0]}/>
+          </label>
+          <input type='radio' id='Pedra' name='choice'/>
+
+          <label for='Papel'>
+            <img src={paper} alt='Papel' className={style[0]}/>
+          </label>
+          <input type='radio' id='Papel' name='choice'/>
+
+          <label for='Tesoura'>
+            <img src={scissors} alt='Tesoura' className={style[0]}/>
+          </label>
+          <input type='radio' id='Tesoura' name='choice'/>
+
+        </div>
+
+        <input type='button' value='Jogar' onClick={click} className='border-2 p-1 bg-[#f0f0f0]'/>
+        
+      </div>
     </div>
   )
 }
